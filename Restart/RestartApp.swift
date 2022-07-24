faa6508ba2a797d90acc1f29210e9e67ba16de9a
Sanghoon <noohgnas.dev@gmail.com>
2022-07-24 21:29:54 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by LEESanghoon on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
